675be6f72c5618a5f25cb562c1d66dea6aa57707
DanMoveo <142913697+DanMoveo@users.noreply.github.com>
2024-07-22 21:41:01 +0300

    adding mobile gallery

--- src/components/pages/Gallery.jsx
+++ src/components/pages/Gallery.jsx
@@ -2,37 +2,36 @@ import React, { useEffect, useState } from 'react';
 import { Carousel } from 'antd';
 import { storage, ref, listAll, getDownloadURL } from '../../firebase';
 import Loader from '../common/Loader';
+import useDeviceType from '../../hooks/useDeviceType';
 
 const Gallery = () => {
-  const [galleryImages, setGalleryImages] = useState([]);
+  const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const isMobile = useDeviceType();
+
   useEffect(() => {
-    const fetchGalleryImages = async () => {
-      const storageRef = ref(storage, 'עמוד גלריה');
+    const fetchImages = async (galleryRef) => {
+      const storageRef = ref(storage, galleryRef);
       const imageRefs = await listAll(storageRef);
       const urls = await Promise.all(
         imageRefs.items.map((item) => getDownloadURL(item))
       );
-      setGalleryImages(urls);
+      setImages(urls);
       setLoading(false);
     };
 
-    fetchGalleryImages();
-  }, []);
+    const galleryRef = isMobile ? 'מובייל גלריה' : 'אינטרנט גלריה';
+    fetchImages(galleryRef);
+  }, [isMobile]);
 
   if (loading) {
     return <Loader />;
   }
 
   return (
-    <Carousel
-      effect='fade'
-      arrows
-      autoplay
-      autoplaySpeed={2000}
-    >
-      {galleryImages.map((src, index) => (
+    <Carousel effect='fade' arrows autoplay autoplaySpeed={2000}>
+      {images.map((src, index) => (
         <div className='h-[70vh] md:h-[80vh]' key={index}>
           <img
             src={src}
