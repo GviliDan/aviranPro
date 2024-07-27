bfa23be4d5bb8483557be61a8a9ee842520ba2a9
DanMoveo <142913697+DanMoveo@users.noreply.github.com>
2024-07-27 12:37:52 +0300

    return to iframe

--- src/components/common/PdfViewer.jsx
+++ src/components/common/PdfViewer.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 const PDFViewer = () => {
   return (
     <div className="w-screen h-screen">
-      <embed
+      <iframe
         src="/aviranPdf.pdf" 
-        type="application/pdf"
         className="w-full h-full"
         style={{ border: 'none' }}
         title="PDF Viewer"
